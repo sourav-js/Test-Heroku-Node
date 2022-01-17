8e6a42fe3b74eb803f32c8c8fa53b1f900655e85
sourav-js <oop.gupta12345@gmail.com>
2022-01-17 21:22:33 +0530

    Changed some code

--- test.js
+++ test.js
@@ -8,7 +8,6 @@ app.get("/",function(req,res){
 
 	res.send("hosted successfully")
 })
-app.listen(port,function(){
-
-	console.log("server started")
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
